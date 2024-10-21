b887f886efe76d911b5335e0cdd0184dd5122d19
ivanaglavinic <ivana.glavinic8@gmail.com>
2024-10-21 16:55:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-alert("Hello");
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  let apiKey = "8ef4a64bf2b3f08b9f692e2febca0acb";
+  let city = searchInputElement.value;
+  cityElement.innerHTML = city;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showTemperature(response) {
+  let temperatureNow = document.querySelector(".temperature-value");
+  let temperature = Math.round(response.data.main.temp);
+  temperatureNow.innerHTML = `${temperature}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
